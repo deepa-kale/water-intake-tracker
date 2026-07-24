396d1a12534546ccbd920a7223917651799aa8bd
deepa-kale <kaledeepa26@gmail.com>
2026-07-24 12:08:48 -0500

    Initial Commit

---
+++ WaterIntakeTracker Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaterIntakeTracker Watch App
+//
+//  Created by Deepa Kale on 7/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaterIntakeTracker Watch App/WaterIntakeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterIntakeTrackerApp.swift
+//  WaterIntakeTracker Watch App
+//
+//  Created by Deepa Kale on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WaterIntakeTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
